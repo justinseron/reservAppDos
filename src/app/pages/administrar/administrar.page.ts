5729d4ab36489f9d90fe3b53511df96fe083c475
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2024-09-05 12:47:22 -0400

    crud con agregar en pagina

--- src/app/pages/administrar/administrar.page.ts
+++ src/app/pages/administrar/administrar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-administrar',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministrarPage implements OnInit {
 
-  constructor() { }
+  persona = new FormGroup({
+    rut: new FormControl('',[Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
+    nombre: new FormControl('',[Validators.required,Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento: new FormControl('',[Validators.required]),
+    genero: new FormControl('',[Validators.required]),
+    tiene_equipo: new FormControl('no',[Validators.required]),
+    nombre_equipo: new FormControl('',[])
+  });
+  usuarios:any[] = [];
+
+  //el servicio nos permite trabajar la información:
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    this.usuarios = this.usuarioService.getUsuarios();
+  }
+
+  registrar(){
+    if( this.usuarioService.createUsuario(this.persona.value) ){
+      alert("USUARIO CREADO CON ÉXITO!");
+      this.persona.reset();
+    }else{
+      alert("ERROR! NO SE PUDO CREAR EL USUARIO!");
+    }
   }
 
 }
