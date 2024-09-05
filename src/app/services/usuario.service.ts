451b10e832c62691914e408ed7dc0da4ac3b831e
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2024-09-05 12:17:57 -0400

    crud usuario listo

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -12,13 +12,39 @@ export class UsuarioService {
 
   //aquí vamos a crear toda nuestra lógica de programación
   //DAO:
-  public createUsuario(usuario:any){}
+  public createUsuario(usuario:any):boolean{
+    if( this.getUsuario(usuario.rut)==undefined ){
+      this.usuarios.push(usuario);
+      return true;
+    }
+    return false;
+  }
 
-  public getUsuario(rut:string){}
+  public getUsuario(rut:string){
+    return this.usuarios.find(elemento=> elemento.rut == rut);
+  }
 
-  public getUsuarios(){}
+  public getUsuarios():any[]{
+    return this.usuarios;
+  }
 
-  public updateUsuario(rut:string, nuevoUsuario:any){}
+  public updateUsuario(rut:string, nuevoUsuario:any){
+    for(let usu of this.usuarios){
+      if(usu.rut==rut){
+        usu = nuevoUsuario;
+        return true;
+      }
+    }
+    return false;
+  }
 
-  public deleteUsuario(rut:string){}
+  public deleteUsuario(rut:string):boolean{
+    const indice = this.usuarios.findIndex(elemento => elemento.rut==rut);
+    if(indice==-1){
+      return false;
+    }
+    this.usuarios.splice(indice,1);
+    return true;
+  }
+
 }
