da7e0401ecb741483b9454b72ace2f898b84dc03
justinseron <ju.seron@duocuc.cl>
2024-10-08 12:50:33 -0300

    Se rescatan varios valores importantes para el viaje (longitud, latitud, etc)

--- src/app/pages/reservas/reservas.page.ts
+++ src/app/pages/reservas/reservas.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 //Lo primero es agregar un import:
 import * as L from 'leaflet';
+import * as G from 'leaflet-control-geocoder';
+import 'leaflet-routing-machine';
 
 @Component({
   selector: 'app-reservas',
@@ -12,6 +14,12 @@ export class ReservasPage implements OnInit {
 
   //Vamos a crear variable(s) para controlar el mapa:
   private map: L.Map | undefined;
+  private geocoder: G.Geocoder | undefined;
+  latitud: number = 0;
+  longitud: number = 0;
+  direccion: string = "";
+  distancia_metros: number = 0;
+  tiempo_segundos: number = 0;
 
   constructor() { }
 
@@ -20,12 +28,43 @@ export class ReservasPage implements OnInit {
   }
 
   initMap(){
-    //this.map = L.map("map_html").locate({setView:true, maxZoom: 16});
-    this.map = L.map("map_html").setView([-33.59839803609597, -70.57884071926678],16);
+    //ACÁ CARGAMOS E INICIALIZAMOS EL MAPA:
+    this.map = L.map("map_html").locate({setView:true, maxZoom: 16});
+    //this.map = L.map("map_html").setView([-33.59839803609597, -70.57884071926678],16);
+
+    //ES LA PLANTILLA PARA QUE SE VEA EL MAPA:
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
+
+    this.map.on('locationfound', (e)=>{
+      console.log(e.latlng.lat);
+      console.log(e.latlng.lng);
+    });
+
+    //VAMOS A AGREGAR UN BUSCADOR DE DIRECCIONES EN EL MAPA:
+    this.geocoder = G.geocoder({
+      placeholder: "Ingrese dirección a buscar",
+      errorMessage: "Dirección no encontrada"
+    }).addTo(this.map);
+
+    //VAMOS A REALIZAR UNA ACCIÓN CON EL BUSCADOR, CUANDO OCURRA ALGO CON EL BUSCADOR:
+    this.geocoder.on('markgeocode', (e)=>{
+      this.latitud = e.geocode.properties['lat'];
+      this.longitud = e.geocode.properties['lon'];
+      this.direccion = e.geocode.properties['display_name'];
+
+      if(this.map){
+        L.Routing.control({
+          waypoints: [L.latLng(-33.59839803609597, -70.57884071926678), L.latLng(this.latitud, this.longitud)],
+          fitSelectedRoutes: true
+        }).on('routesfound', (e)=>{
+          this.distancia_metros = e.routes[0].summary.totalDistance;
+          this.tiempo_segundos = e.routes[0].summary.totalTime;
+        }).addTo(this.map);
+      }
+    });
   }
 
 }
